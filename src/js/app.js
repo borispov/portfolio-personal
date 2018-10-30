25b2b6b05e5ad89259e55af364202af5ba087796
Boris Povolotsky <boristofu@gmail.com>
2018-10-31 01:11:12 +0200

    version 1.0.0 --A: SmoothScroll. Description: all links are functional. arrowscroll as well. N: rdy for deploy

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,65 @@
+const smoothScroll = () => {
+  
+  const aLinks = document.querySelectorAll('.-scroll')
+
+  // assign eventHandler to each of them.
+  aLinks.forEach(node => {
+    node.addEventListener('click', e => {
+      e.preventDefault()
+
+      const targetId =
+        e.target.localName === 'button'
+          ? node.getAttribute('href').split('#')[1]
+          : // e.target.baseURI.split('#')[1]
+          e.target.href.split('#')[1]
+
+      const targetNode = document.getElementById(targetId)
+
+      const anim = requestAnimationFrame(timestamp => {
+        const stamp = timestamp || new Date().getTime()
+        const duration = 1200
+        const start = stamp
+
+        const startOffset = window.pageYOffset
+
+        const scrollEndElemTop = targetNode.getBoundingClientRect().top
+
+        scrollToNode(start, stamp, duration, scrollEndElemTop, startOffset)
+      })
+    })
+  })
+
+  // Gurjit's Magic .!.
+
+  const easyInCubic = t => t * t * t
+  // Gurjit's Magic function
+  const scrollToNode = (startTime, currentTime, dur, targetPos, startPos) => {
+    const runtime = currentTime - startTime
+    let progress = runtime / dur
+
+    progress = Math.min(progress, 1)
+
+    const ease = easyInCubic(progress)
+
+    window.scroll(0, startPos + targetPos * ease)
+
+    if (runtime < dur) {
+      requestAnimationFrame(timestamp => {
+        const currentTime = timestamp || new Date().getTime()
+        scrollToNode(startTime, currentTime, dur, targetPos, startPos)
+      })
+    }
+  }
+}
+
+window.onload = function() {
+  smoothScroll()
+  const arrowScroll = document.querySelectorAll('.downarrow')
+  for (x of arrowScroll) {
+    let xHref = x.parentElement.id === 'home' ? 'portfolio' : 'portfolio2'
+    x.firstElementChild.setAttribute('href', `#${xHref}`)
+  }
+}
 
 const navLink = document.querySelectorAll('.nav__link')
 
